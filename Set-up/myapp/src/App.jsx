c6af81dde45792f6d5507d5014b385e571f90691
Sup-ri-tha <supritharajashekar10@gmail.com>
2025-02-19 12:19:11 +0530

    For deploying

---
+++ Set-up/myapp/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+//import Home from  './Home.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+            <h1>Name: Supritha Rajashekar</h1>
+            <p>Contact: supritha@gmail.com</p>
+        </div>
+    </>
+  )
+}
+
+export default App;
