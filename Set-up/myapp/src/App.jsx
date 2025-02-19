375a4c909902d056370c576b92a26fb6693d084b
Sup-ri-tha <supritharajashekar10@gmail.com>
2025-02-19 13:15:20 +0530

    Added Home Page

--- Set-up/myapp/src/App.jsx
+++ Set-up/myapp/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import './App.css'
-//import Home from  './Home.jsx';
+import {Home} from  './Home.jsx';
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-            <h1>Name: Supritha Rajashekar</h1>
-            <p>Contact: supritha@gmail.com</p>
-        </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />       
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ Set-up/myapp/src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Name: Supritha Rajashekar</h1>
+        <p>Contact: supritha@gmail.com</p>
+    </div>
+  )
+}
